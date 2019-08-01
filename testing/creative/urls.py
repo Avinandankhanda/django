f49228482ef4041c6dc1259921c3022bb7408e20
Avinandan khanda <avinandankhanda2002>
2019-08-01 21:16:24 +0530

    context ver

--- testing/creative/urls.py
+++ testing/creative/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.views.generic import TemplateView
 from . import views
 urlpatterns= [
-    path("",views.home,name="home"),                      # route
+    path("",views.index,name="home"),                      # route
     path("menu/",views.menu,name="menu"),
-    path("hotel/",TemplateView.as_view(template_name="hotel.html"),name="hotel")
+    path("khan/",TemplateView.as_view(template_name="khan.html"),name="hotel"),
+    path("new/",views.practice,name="practice"),
+    path("recall/",views.recall,name="rrrrr")
 ]
--- testing/creative/views.py
+++ testing/creative/views.py
@@ -1,9 +1,31 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Welcome to the world of Avinandan Khanda")
 
 def menu(request):
     return HttpResponse("it's my web-site")
+
 def new(request):
     return HttpResponse("no body can see it")
+
+def practice(request): 
+    html="""
+   
+    <h1>it is my firt sight if any disapointment please contact me</h1>
+    """
+    return HttpResponse(html)
+def recall(request):
+    html="""
+    
+    <h1>i am re calling the project<h1/>
+    """
+    return HttpResponse(html)
+def index(request):
+    template_name="index.html"
+    context={
+        "name":'Avinandan Khanda',
+        "age":17
+            }
+    return render(request, template_name,context)
